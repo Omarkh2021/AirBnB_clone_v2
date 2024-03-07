3b4a9c51f1b3b1ae497acf50e856602a33570acc
Omarkh2021 <90703502+Omarkh2021@users.noreply.github.com>
2024-03-07 11:24:22 +0200

    Update 1-hbnb_route.py

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,25 +1,44 @@
 #!/usr/bin/python3
-'''A simple Flask web application.
-'''
+""" Script that starts a Flask web application:
+
+    Requirements:
+    - web application must be listening on 0.0.0.0, port 5000
+    - You must use the option strict_slashes=False in your route definition
+    Routes:
+    /: display “Hello HBNB!”
+    /hbnb: display “HBNB”
+"""
+
+# Imports the Flask module and creates a Flask web server from the Flask module
 from flask import Flask
 
-
-app = Flask(__name__)
-'''The Flask application instance.'''
-app.url_map.strict_slashes = False
+#  Creates a new web application object, which we’ll use to configure routes.
+app = Flask("__name__")
 
 
-@app.route('/')
-def index():
-    '''The home page.'''
-    return 'Hello HBNB!'
+# -Decorator tells Flask what URL should trigger our function.
+# -The strict_slashes=False option means the route is defined without
+#   trailing slashes and Flask redirects all URLs with trailing slashes
+#   to this route.
+#  - def hello() This function is triggered when the user visits the root URL.
+#    It returns the string ‘Hello HBNB!’.
+@app.route('/', strict_slashes=False)
+def Disp_hello():
+    """Display 'Hello HBNB!."""
+    return ("Hello HBNB!")
 
 
-@app.route('/hbnb')
-def hbnb():
-    '''The hbnb page.'''
-    return 'HBNB'
+# -Decorator tells Flask to trigger the hbnb() function when the ‘/hbnb’ URL
+#  is visited.
+@app.route("/hbnb", strict_slashes=False)
+# -This function is triggered when the user visits the ‘/hbnb’ URL.
+#  It returns the string ‘HBNB’.
+def Disp_hbnb():
+    """Returns a given string"""
+    return ("HBNB")
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000')
+# -If this script is run directly (not imported), then it will run the app
+#   on host 0.0.0.0 (which makes it publicly available) and on port 5000.
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=None)
