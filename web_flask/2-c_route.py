c0418820d75ad5c278e706a8f0768f9bca1b06d4
Omarkh2021 <90703502+Omarkh2021@users.noreply.github.com>
2024-03-07 11:26:10 +0200

    Update 2-c_route.py

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,31 +1,53 @@
 #!/usr/bin/python3
-'''A simple Flask web application.
-'''
+"""Script that starts a Flask web application:
+
+Requirements:
+    - Web application must be listening on 0.0.0.0, port 5000
+    - Routes:
+      /: display “Hello HBNB!”.
+      /hbnb: display “HBNB”.
+      /c/<text>: display “C ” followed by the value of the text variable
+     (replace underscore _ symbols with a space ).
+    - You must use the option strict_slashes=False in your route definition
+
+"""
+
+# Imports the Flask module and creates a Flask web server from the Flask module
 from flask import Flask
 
-
+# Creates a new web application object, which we’ll use to configure routes.
 app = Flask(__name__)
-'''The Flask application instance.'''
-app.url_map.strict_slashes = False
 
 
-@app.route('/')
-def index():
-    '''The home page.'''
+# -This Decorator tells Flask what URL should trigger our function.
+# -strict_slashes=False: option means the route is defined without trailing
+# slashes and Flask redirects all URLs with trailing slashes to this route.
+@app.route('/', strict_slashes=False)
+def Disp_hello_HBNB():
+    """This function is triggered when the user visits the root URL"""
     return 'Hello HBNB!'
 
 
-@app.route('/hbnb')
-def hbnb():
-    '''The hbnb page.'''
+# -This decorator tells Flask to trigger the show_HBNB() function when
+#  the ‘/hbnb’ URL is visited.
+@app.route('/hbnb', strict_slashes=False)
+def Disp_HBNB():
+    """This function is triggered when the user visits the ‘/hbnb’ URL"""
     return 'HBNB'
 
 
-@app.route('/c/<text>')
-def c_page(text):
-    '''The c page.'''
+# -This decorator tells Flask to trigger the show_C(text) function when the
+#  ‘/c/<text>’ URL is visited. <text> is a variable part of the URL.
+# -Returns a string that starts with 'C ’ and ends with the <text> from
+#   the URL, with underscores replaced by spaces.
+@app.route('/c/<text>', strict_slashes=False)
+def Disp_C(text):
+    """This function is triggered when the user visits the ‘/c/<text>’ URL"""
     return 'C {}'.format(text.replace('_', ' '))
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000')
+# -If this script is run directly (not imported), then it will run the app
+#   on host 0.0.0.0 (which makes it publicly available) and on port 5000.
+# Set socket to run the app on.
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
