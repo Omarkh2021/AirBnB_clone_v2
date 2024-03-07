0f7762fdd6fa9f4427a64f406bc9f53ba656aed4
Omarkh2021 <90703502+Omarkh2021@users.noreply.github.com>
2024-03-07 11:18:37 +0200

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -1,67 +1,56 @@
 #!/usr/bin/python3
-"""This module defines a base class for all models in our hbnb clone"""
-import os
+"""This is the Base Model module"""
+
 import uuid
+from models import storage
 from datetime import datetime
-from sqlalchemy import Column, String, DATETIME
-from sqlalchemy.ext.declarative import declarative_base
-
-
-Base = declarative_base()
 
 
 class BaseModel:
-    """A base class for all hbnb models"""
-    id = Column(String(60), nullable=False, primary_key=True, unique=True)
-    created_at = Column(DATETIME, nullable=False, default=datetime.utcnow())
-    updated_at = Column(DATETIME, nullable=False, default=datetime.utcnow())
+    """This is the BaseModel class that defines common
+       attributes and methods for other classes.
+    """
 
     def __init__(self, *args, **kwargs):
-        """Instantiates a new model"""
+        """Code block for initializing the class.
+        Args:
+            *args: unused
+            **kwargs (dict): Dictionnary containing wanted attributes
+        """
         if not kwargs:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
-            self.updated_at = datetime.now()
+            self.updated_at = self.created_at
+            storage.new(self)
         else:
-            for key, value in kwargs.items():
-                if key != '__class__':
-                    if key in ('created_at', 'updated_at'):
-                        setattr(self, key, datetime.fromisoformat(value))
-                    else:
-                        setattr(self, key, value)
-            # if os.getenv('HBNB_TYPE_STORAGE') in ('db'):
-            if not hasattr(kwargs, 'id'):
-                setattr(self, 'id', str(uuid.uuid4()))
-            if not hasattr(kwargs, 'created_at'):
-                setattr(self, 'created_at', datetime.now())
-            if not hasattr(kwargs, 'updated_at'):
-                setattr(self, 'updated_at', datetime.now())
+            for k, v in kwargs.items():
+                if k in ["created_at", "updated_at"]:
+                    v = datetime.strptime(v, "%Y-%m-%dT%H:%M:%S.%f")
+                if k != "__class__":
+                    setattr(self, k, v)
 
     def __str__(self):
-        """Returns a string representation of the instance"""
-        cls = (str(type(self)).split('.')[-1]).split('\'')[0]
-        return '[{}] ({}) {}'.format(cls, self.id, self.__dict__)
-
-    def delete(self):
-        """Deletes this BaseModel instance from the storage"""
-        from models import storage
-        storage.delete(self)
+        """Method to obtain a string representation
+        of the selected attribute.
+        """
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
 
     def save(self):
-        """Updates updated_at with current time when instance is changed"""
-        from models import storage
+        """Method for refreshing the 'updated_at' public
+           instance attribute with the current timestamp.
+        """
         self.updated_at = datetime.now()
-        storage.new(self)
         storage.save()
 
     def to_dict(self):
-        """Convert instance into dict format"""
-        res = {}
-        for key, value in self.__dict__.items():
-            if key != '_sa_instance_state':
-                if isinstance(value, datetime):
-                    res[key] = value.isoformat()
-                else:
-                    res[key] = value
-        res['__class__'] = self.__class__.__name__
-        return res
+        """ Method to retrieve a dictionary containing
+        all attributes and their values for the instance.
+        """
+        new_dict = {}
+        for k, v in self.__dict__.items():
+            if k in ["created_at", "updated_at"]:
+                new_dict[k] = v.isoformat()
+            else:
+                new_dict[k] = v
+        new_dict['__class__'] = self.__class__.__name__
+        return new_dict
