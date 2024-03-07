12b76a1adce598ff141fe8d159c33122bed81c1a
Omarkh2021 <90703502+Omarkh2021@users.noreply.github.com>
2024-03-07 11:28:04 +0200

    Update 7-states_list.py

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,33 +1,55 @@
 #!/usr/bin/python3
-'''A simple Flask web application.
-'''
-from flask import Flask, render_template
-
+"""Script that starts a Flask web application:
+- Your web application must be listening on 0.0.0.0, port 5000.
+- You must use storage for fetching data from the storage engine (FileStorage
+   or DBStorage) => from models import storage and storage.all(...).
+- After each request you must remove the current SQLAlchemy Session:
+    -Declare a method to handle @app.teardown_appcontext
+    -Call in this method storage.close()
+- Routes:
+    -/states_list: display a HTML page: (inside the tag BODY).
+    -H1 tag: “States”.
+    -UL tag: with the list of all State objects present in DBStorage sorted
+      by name (A->Z) tip.
+    -LI tag: description of one State: <state.id>: <B><state.name></B>
+- Import this 7-dump to have some data
+- You must use the option strict_slashes=False in your route definition
+"""
 from models import storage
+from flask import Flask, render_template
 from models.state import State
 
-
+# Creates an instance of a Flask web application.
 app = Flask(__name__)
-'''The Flask application instance.'''
-app.url_map.strict_slashes = False
 
 
-@app.route('/states_list')
-def states_list():
-    '''The states_list page.'''
-    all_states = list(storage.all(State).values())
-    all_states.sort(key=lambda x: x.name)
-    ctxt = {
-        'states': all_states
-    }
-    return render_template('7-states_list.html', **ctxt)
+# -Defines a route for the web application. When a user navigates to
+# <your-domain>/states_list in their web browser, the function defined
+# directly below this line will be executed.
+@app.route("/states_list", strict_slashes=False)
+def Disp_states():
+    """This function is executed when a user navigates to the /states_list
+    route. It retrieves all “State” objects from the storage, and then uses
+    the render_template function to generate an HTML page that lists all of
+    these states.
+    """
+    states = storage.all()
+    return render_template("7-states_list.html", states=states)
 
 
+# -This decorator ensures that the function defined directly below it
+# (def teardown(exc)) will be called when the application context ends,
+#  which typically occurs after a request has been processed.
 @app.teardown_appcontext
-def flask_teardown(exc):
-    '''The Flask app/request context end event listener.'''
+def teardown(exc):
+    """
+    This function is called after each request, and it closes the storage
+    session.
+    """
     storage.close()
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000')
+# -Starts the Flask web server if the script is being run directly
+# (as opposed to being imported as a module).
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
