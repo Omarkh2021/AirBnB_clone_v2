31569d6f9cdd0a7bd72aff8ed2f2eeb9b2d8397c
Omarkh2021 <90703502+Omarkh2021@users.noreply.github.com>
2024-03-07 12:25:36 +0200

    Update 1-pack_web_static.py

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,29 +1,21 @@
 #!/usr/bin/python3
-"""A module for web application deployment with Fabric."""
-import os
+# Fabric script that generates a .tgz archive from the contents of the
+# web_static folder of your AirBnB Clone repo,using the function do_pack.
+
+from os.path import isdir
 from datetime import datetime
-from fabric.api import local, runs_once
+from fabric.api import local
 
 
-@runs_once
 def do_pack():
-    """Archives the static files."""
-    if not os.path.isdir("versions"):
-        os.mkdir("versions")
-    cur_time = datetime.now()
-    output = "versions/web_static_{}{}{}{}{}{}.tgz".format(
-        cur_time.year,
-        cur_time.month,
-        cur_time.day,
-        cur_time.hour,
-        cur_time.minute,
-        cur_time.second
+    """Create a tar gzipped archive of the directory web_static."""
+    file_name = "versions/web_static_{}.tgz".format(
+        datetime.now().strftime("%Y%-m%-d%-H%-M%-S")
     )
-    try:
-        print("Packing web_static to {}".format(output))
-        local("tar -cvzf {} web_static".format(output))
-        archize_size = os.stat(output).st_size
-        print("web_static packed: {} -> {} Bytes".format(output, archize_size))
-    except Exception:
-        output = None
-    return output
+
+    if isdir("versions") is False:
+        if local("mkdir -p versions").failed is True:
+            return None
+    if local("tar -cvzf {} web_static".format(file_name)).failed is True:
+        return None
+    return
